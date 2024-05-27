0f3df493baeb0508741f131a398c51108e51d72b
manucasta04 <mcastagnari@fi.uba.ar>
2024-05-26 22:52:34 -0300

    acciones de la tortuga implementadas y main arreglado

--- proyectotp2/src/proyectotp2/main.clj
+++ proyectotp2/src/proyectotp2/main.clj
@@ -1,36 +1,58 @@
 (ns proyectotp2.main)
 (require '[clojure.string :as str])
-
-(defn -main [read_file num_iterations write_file]
-  (println read_file num_iterations write_file "Main.")
-  (def pila (Pila. [(Tortuga. (vector 0 0) 0 true)]))
-  (def sistema-l-especificaciones (str/split (slurp read_file) #"\n"))
-  (def angulo-giro (first sistema-l-especificaciones))
-  (def axioma (first (rest sistema-l-especificaciones)))
-  (def pares-predecesor-sucesor (map seq (map #(str/split % #" ") (rest (rest sistema-l-especificaciones)))))
-  (def sistema-l (apply ->Sistema-L axioma angulo-giro pares-predecesor-sucesor))
-  (funcion-recursiva num_iterations pila sistema-l write_file))
+(require '[clojure.math :as math])
+(java.util.Locale/setDefault java.util.Locale/US)
 
 (defn funcion-recursiva
   [i pila-tortugas sistema-l ruta-svg]
   (if (zero? i)
-    nil
+    (spit ruta-svg "</svg>" :append true)
     (funcion-recursiva pila-tortugas sistema-l ruta-svg (dec i))))
+
+(defn -main [read_file num_iterations write_file]
+  (println read_file num_iterations write_file "Main.")
+  (spit write_file "<svg viewBox=\"-50 -150 300 200\" xmlns=\"http://www.w3.org/2000/svg\">")
+  (let [pila (Pila. [(Tortuga. (vector 0.0 0.0) 0.0 true)])
+    sistema-l-especificaciones (str/split (slurp read_file) #"\n")
+    angulo-giro (first sistema-l-especificaciones)
+    axioma (first (rest sistema-l-especificaciones))
+    pares-predecesor-sucesor (apply merge (map #(hash-map (keyword (first %1)) (get (vec (rest %1)) 0)) (vec (map #(str/split % #" ") (rest (rest sistema-l-especificaciones))))))
+    sistema-l (Sistema-L. axioma angulo-giro pares-predecesor-sucesor)]
+    (funcion-recursiva num_iterations pila sistema-l write_file)))
+
+(defn escribir-svg
+  [archivo punto-a punto-b]
+  (spit archivo (format "<line x1=\"%.1f\" y1=\"%.1f\" x2=\"%.1f\" y2=\"%.1f\" stroke-width=\"1\" stroke=\"black\" />" (get punto-a 0) (get punto-a 1) (get punto-b 0) (get punto-b 1)) :append true))
 
 (defprotocol TortugaAcciones
   (avanzar [tortuga cantidad archivo])
-  (izquierda [tortuga angulo])
-  (derecha [tortuga angulo])
+  (izquierda [tortuga angulo-giro])
+  (derecha [tortuga angulo-giro])
   (pluma-abajo [tortuga])
   (pluma-arriba [tortuga]))
 
 (defrecord Tortuga [posicion angulo pluma-arriba?]
   TortugaAcciones
-  (avanzar [tortuga cantidad archivo] ())
-  (izquierda [tortuga angulo] ())
-  (derecha [tortuga angulo] ())
-  (pluma-abajo [tortuga] ())
-  (pluma-arriba [tortuga] ()))
+  (avanzar 
+  [tortuga cantidad archivo]
+  (let [direction (vector (math/cos (math/to-radians (:angulo tortuga))) (math/sin (math/to-radians (:angulo tortuga))))
+    new-position (vector (+ (get (:posicion tortuga) 0) (* (get direction 0) cantidad)) (+ (get (:posicion tortuga) 1) (* (get direction 1) cantidad)))]
+    (if (:pluma-arriba? tortuga)
+      nil
+      (escribir-svg archivo (:posicion tortuga) new-position))
+    (Tortuga. new-position (:angulo tortuga) (:pluma-arriba? tortuga))))
+  (izquierda 
+  [tortuga angulo-giro] 
+  (Tortuga. (:posicion tortuga) (- (:angulo tortuga) angulo-giro) (:pluma-arriba? tortuga)))
+  (derecha 
+  [tortuga angulo-giro] 
+  (Tortuga. (:posicion tortuga) (+ (:angulo tortuga) angulo-giro) (:pluma-arriba? tortuga)))
+  (pluma-abajo [tortuga] 
+  (Tortuga. (:posicion tortuga) (:angulo tortuga)  false))
+  (pluma-arriba 
+  [tortuga] 
+  (Tortuga. (:posicion tortuga) (:angulo tortuga) true)))
+
 
 (defprotocol PilaAcciones
   (apilar [pila elemento])
@@ -46,6 +68,6 @@
 (defprotocol Sistema-LAcciones
   (suceder [sistema-l]))
 
-(defrecord Sistema-L [axioma angulo-giro & pares-predecesor-sucesor]
+(defrecord Sistema-L [axioma angulo-giro pares-predecesor-sucesor]
   Sistema-LAcciones
   (suceder [sistema-l] ()))
