515c70c29b672e1551ed16f2d142a11bf3c843c0
LtDeno <ldenobili@fi.uba.ar>
2024-05-29 15:23:34 -0300

    los parametros del viewbox se ajustan a las lineas para poder contenerlas

--- proyectotp2/src/proyectotp2/filemanager.clj
+++ proyectotp2/src/proyectotp2/filemanager.clj
@@ -25,17 +25,53 @@
 (defn escribir-primer-linea
   "Escribe la primera linea para un archivo SVG en el archivo especificado.
   Sobreescribe el archivo."
-  [write_file]
-  (spit write_file "<svg viewBox=\"-900 -900 1500 1500\" preserveAspectRatio=\"xMidYMid meet\" xmlns=\"http://www.w3.org/2000/svg\">"))
+  [write_file x_min y_min x_max y_max]
+  (spit write_file (format "<svg viewBox=\"%.1f %.1f %.1f %.1f\" preserveAspectRatio=\"xMidYMid meet\" xmlns=\"http://www.w3.org/2000/svg\">\n" x_min y_min x_max y_max)))
 
 (defn escribir-linea-vector
   "Escribe una linea (tal que dos vectores posicion) en formato SVG en el archivo especificado.
   No sobreescribe el archivo."
   [write_file start_pos end_pos]
-  (spit write_file (format "\n<line x1=\"%.1f\" y1=\"%.1f\" x2=\"%.1f\" y2=\"%.1f\" stroke-width=\"1\" stroke=\"black\" />" (get start_pos 0) (get start_pos 1) (get end_pos 0) (get end_pos 1)) :append true))
+  (spit write_file (format "<line x1=\"%.1f\" y1=\"%.1f\" x2=\"%.1f\" y2=\"%.1f\" stroke-width=\"1\" stroke=\"black\"/>\n" (get start_pos 0) (get start_pos 1) (get end_pos 0) (get end_pos 1)) :append true))
 
 (defn escribir-ultima-linea
   "Escribe la ultima linea para un archivo SVG en el archivo especificado.
   No sobreescribe el archivo."
   [write_file]
   (spit write_file "\n</svg>" :append true))
+
+(defn conseguir-floats
+  "Devuelve un vector de floats obtenidos de leer el String como si fuese un elemento linea de SVG."
+  [string]
+  (vector (Float/parseFloat (get (str/split string #"\"") 1))
+          (Float/parseFloat (get (str/split string #"\"") 3))
+          (Float/parseFloat (get (str/split string #"\"") 5))
+          (Float/parseFloat (get (str/split string #"\"") 7))))
+
+(defn vector-maximizado
+  "Devuelve un vector con los valores maximizados (en positivo y negativo) de ambos vectores"
+  [vect_1 vect_2]
+  (vector (min (get vect_1 0) (get vect_2 0) (get vect_2 2))
+          (min (get vect_1 1) (get vect_2 1) (get vect_2 3))
+          (max (get vect_1 0) (get vect_2 0) (get vect_2 2))
+          (max (get vect_1 1) (get vect_2 1) (get vect_2 3))))
+
+(defn obtener-valores-viewbox
+  "Devuelve un vector ordenado segun x_min y_min ancho alto, cuyos valores son obtenidos del array de elementos linea pasados."
+  [vect_elementos_linea]
+  (reduce #(vector-maximizado %1 (conseguir-floats %2)) [(float 0) (float 0) (float 0) (float 0)] vect_elementos_linea))
+
+(defn ajustar-viewbox
+  "Modifica la primer linea del SVG guardado, cambiando el tamaño del viewBox para que contenga las lineas generadas
+  Sobreescribe el archivo pero modificando la primer linea, el resto intacto.
+  No contempla la existencia de una ultima linea de cierre de SVG."
+  [write_file]
+  (let [vect_archivo (str/split-lines (slurp write_file))
+        vect_elementos_linea (rest vect_archivo)
+        vect_margenes_viewbox (obtener-valores-viewbox vect_elementos_linea)]
+    (escribir-primer-linea write_file
+                           (- (get vect_margenes_viewbox 0) 50.0)
+                           (- (get vect_margenes_viewbox 1) 50.0)
+                           (+ (get vect_margenes_viewbox 2) 100.0)
+                           (+ (get vect_margenes_viewbox 3) 100.0))
+    (spit write_file (str/join "\n" vect_elementos_linea) :append true)))
--- proyectotp2/src/proyectotp2/main.clj
+++ proyectotp2/src/proyectotp2/main.clj
@@ -10,6 +10,7 @@
         cmd_seq (sisl/transformar-axioma (fman/obtener-axioma read_vect) (fman/obtener-reglas read_vect) (Integer/parseInt num_iterations))
         turn_angle (fman/obtener-angulo read_vect)]
 
-    (fman/escribir-primer-linea write_file)
+    (fman/escribir-primer-linea write_file -50.0 -50.0 100.0 100.0)
     (sisl/procesar cmd_seq (Float/parseFloat turn_angle) write_file)
+    (fman/ajustar-viewbox write_file)
     (fman/escribir-ultima-linea write_file)))
